0689bbdb38335a386781aa1e0a2c3cfa0942fff1
olawhale <57977842+olawhale@users.noreply.github.com>
2026-04-09 05:43:37 +0100

    Update App.tsx

--- Appo.Frontend/src/App.tsx
+++ Appo.Frontend/src/App.tsx
@@ -1,1 +1,18 @@
+import { Dashboard } from "./components/Dashboard";
+import { WorkloadTable } from "./components/WorkloadTable";
+import { ForecastChart } from "./components/ForecastChart";
+import "./App.css";
 
+function App() {
+  return (
+    <div className="app">
+      <Dashboard />
+      <hr />
+      <WorkloadTable />
+      <hr />
+      <ForecastChart />
+    </div>
+  );
+}
+
+export default App;
