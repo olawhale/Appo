20df753f8e3b9d4f9988418b5f86d7435d2cd811
olawhale <57977842+olawhale@users.noreply.github.com>
2026-04-09 04:39:01 +0100

    Update PecSummary.cs

--- Appo.Backend/Models/PecSummary.cs
+++ Appo.Backend/Models/PecSummary.cs
@@ -1,1 +1,9 @@
+namespace Appo.Backend.Models;
 
+public class PecSummary
+{
+    public decimal CurrentPec { get; set; }
+    public decimal MissedPec { get; set; }
+    public decimal UpliftPotential { get; set; }
+    public int PecScore { get; set; }
+}
