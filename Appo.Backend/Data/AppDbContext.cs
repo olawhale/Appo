714d823eca551c81814727ef784a72c67b0aa7f6
olawhale <57977842+olawhale@users.noreply.github.com>
2026-04-09 05:29:18 +0100

    Update AppDbContext.cs

--- Appo.Backend/Data/AppDbContext.cs
+++ Appo.Backend/Data/AppDbContext.cs
@@ -3,5 +3,6 @@ using Microsoft.EntityFrameworkCore;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
-    public DbSet<Something> Somethings { get; set; }
+   public DbSet<Workload> Workloads { get; set; }
+public DbSet<Recommendation> Recommendations { get; set; }
 }
