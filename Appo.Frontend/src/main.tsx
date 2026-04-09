eb064ce6c07c7c90900d13fee835b35ccb853312
olawhale <57977842+olawhale@users.noreply.github.com>
2026-04-09 07:43:49 +0100

    Update main.tsx

--- Appo.Frontend/src/main.tsx
+++ Appo.Frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
 );
